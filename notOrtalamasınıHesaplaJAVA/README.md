8ee24f599010230a1eefbfbd992c84e133b3d0ee
Mevlüt Çelik <23mevlutcelik@gmail.com>
2022-04-12 17:15:28 +0300

    Not ortalamasını hesaplayan proje ödevi yapıldı

---
+++ notOrtalamasınıHesaplaJAVA/README.md
@@ -0,0 +1,5 @@
+## Not ortalamasını hesaplayan program
+```
+ÖDEV (JAVA)
+```
+Java ile Matematik, Fizik, Kimya, Türkçe, Tarih, Müzik derslerinin sınav puanlarını kullanıcıdan alan ve ortalamalarını hesaplayıp ekrana bastırılan programı yazın.
---
+++ notOrtalamasınıHesaplaJAVA/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        // Değişkenleri oluştur
+        int matematik, fizik, kimya, turkce, tarih, muzik;
+        // Ekrana başlık gösterdik
+        System.out.println("###########################################");
+        System.out.println("### NOT ORTALAMASINI HESAPLA ###");
+        // Scanner sınıfını tanımladık
+        Scanner input = new Scanner(System.in);
+        // Kullanıcıdan değerleri al
+        System.out.print("Matematik notunuzu giriniz: ");
+        matematik = input.nextInt();
+        System.out.print("Fizik notunuzu giriniz: ");
+        fizik = input.nextInt();
+        System.out.print("Kimya notunuzu giriniz: ");
+        kimya = input.nextInt();
+        System.out.print("Türkçe notunuzu giriniz: ");
+        turkce = input.nextInt();
+        System.out.print("Tarih notunuzu giriniz: ");
+        tarih = input.nextInt();
+        System.out.print("Müzik notunuzu giriniz: ");
+        muzik = input.nextInt();
+        int toplam = matematik + fizik + kimya + turkce + tarih + muzik;
+        double sonuc = toplam / 6;
+        System.out.println("###########################################");
+        System.out.print("## Not ortalamanız: " + sonuc);
+    }
+}
